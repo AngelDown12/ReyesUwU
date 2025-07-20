87c2b128fed317bbc14c96d0bbb52fcd3eb41c8d
AngelDown12 <angeldown12xz@gmail.com>
2025-07-20 00:33:50 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,65 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Sofi 👸🏻;;\nFN:Sofi 👸🏻\nORG:Sofi 👸🏻\nTITLE:\nitem1.TEL;waid=50765609370:50765609370\nitem1.X-ABLabel:Sofi 👸🏻\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Sofi 👸🏻\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+import PhoneNumber from 'awesome-phonenumber'
 
+let handler = async (m, { conn }) => {
+  if (m.quoted?.fromMe || m.isButton) return
+
+  m.react('🧨')
+
+  const imageUrl = 'https://files.catbox.moe/iydxk1.jpg'
+  const numCreador = '5217227584934'
+  const ownerJid = numCreador + '@s.whatsapp.net'
+
+  const name = '𝐀𝐍𝐆𝐄𝐋 🧨'
+  const about = '𝐒𝐨𝐲 𝐀𝐧𝐠𝐞𝐥, 𝐃𝐮𝐞𝐧̃𝐨 𝐝𝐞𝐥 𝐁𝐨𝐭 𝐀𝐧𝐠𝐞𝐥 𝐛𝐨𝐭 🧨'
+  const empresa = '𝐀𝐧𝐠𝐞𝐥 - 𝐒𝐞𝐫𝐯𝐢𝐜𝐢𝐨𝐬 𝐭𝐞𝐜𝐧𝐨𝐥𝐨́𝐠𝐢𝐜𝐨𝐬 🧨'
+  const instagramUrl = 'https://www.instagram.com/bki_hm66'
+
+  const vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:${instagramUrl}
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD`.trim()
+
+  await conn.sendMessage(
+    m.chat,
+    {
+      contacts: {
+        displayName: name,
+        contacts: [{ vcard }]
+      },
+      contextInfo: {
+        mentionedJid: [m.sender],
+        externalAdReply: {
+          title: '𝐀𝐍𝐆𝐄𝐋 𝐁𝐎𝐓 🧨',
+          body: '𝐀𝐍𝐆𝐄𝐋 🧨',
+          thumbnailUrl: imageUrl,
+          sourceUrl: instagramUrl,
+          mediaType: 1,
+          showAdAttribution: true,
+          renderLargerThumbnail: true
+        }
+      }
+    },
+    { quoted: m }
+  )
+}
+
+handler.help = ['owner']
+handler.tags = ['owner']
+handler.command = /^\.owner$/i
+handler.register = false
 export default handler
